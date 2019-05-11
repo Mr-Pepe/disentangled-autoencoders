d85008c8701e88af6a288bcac3723fcbbadefe8a
Felix Meissen <felix.meissen@gmail.com>
2019-05-11 12:21:39 +0200

    Add ResizeConvLayer to avoid checkerboard artifact

--- dl4cv/models/decoder.py
+++ dl4cv/models/decoder.py
@@ -3,7 +3,7 @@ File to store all decoder architectures
 """
 import torch.nn as nn
 
-from dl4cv.models.model_utils import ResidualBlock, Conv2dReflectionPadding
+from dl4cv.models.model_utils import ResidualBlock, ResizeConvLayer
 
 
 class VanillaDecoder(nn.Module):
@@ -13,20 +13,27 @@ class VanillaDecoder(nn.Module):
         self.res1 = ResidualBlock(256)
         self.res2 = ResidualBlock(256)
 
-        # Transposed convolutions for upsampling
-        # Convolutional layers are used to prevent checkerboard patterns
-        self.t_conv1 = nn.ConvTranspose2d(in_channels=256, out_channels=256, kernel_size=4, stride=2,
-                                            padding=1, output_padding=0)
-        self.conv1  = Conv2dReflectionPadding(in_channels=256,out_channels=256, kernel_size=3, stride=1, padding=1)
-        self.t_conv2 = nn.ConvTranspose2d(in_channels=256, out_channels=3, kernel_size=4, stride=2,
-                                            padding=1, output_padding=0)
-        self.conv2 = Conv2dReflectionPadding(in_channels=3, out_channels=3, kernel_size=3, stride=1, padding=1)
+        # Resize convolutional layers are used to prevent checkerboard patterns
+        self.resizeConv1 = ResizeConvLayer(
+            in_channels=256,
+            out_channels=256,
+            kernel_size=4,
+            padding=1,
+            stride=1,
+            scale_factor=2
+        )
+        self.resizeConv2 = ResizeConvLayer(
+            in_channels=256,
+            out_channels=3,
+            kernel_size=4,
+            padding=1,
+            stride=1,
+            scale_factor=2
+        )
 
     def forward(self, x):
         y = self.res1(x)
         y = self.res2(y)
-        y = self.t_conv1(y)
-        y = self.conv1(y)
-        y = self.t_conv2(y)
-        y = self.conv2(y)
+        y = self.resizeConv1(y)
+        y = self.resizeConv2(y)
         return y
