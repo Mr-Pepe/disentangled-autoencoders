786c6260848012d3e5a7207dc19c276377d0eccd
Felix Meissen <felix.meissen@gmail.com>
2019-05-13 22:38:48 +0200

    adapt code-style for my small screen

--- dl4cv/showModelOutput.py
+++ dl4cv/showModelOutput.py
@@ -1,50 +1,55 @@
-import matplotlib
 import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
 import numpy as np
-from PIL import Image, ImageChops
 import torch
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
-from torch.utils.data.sampler import SequentialSampler, SubsetRandomSampler
-import pickle
+from torch.utils.data.sampler import SequentialSampler
+
 
 config = {
 
-    'data_path': '/home/felipe/Projects/dl4cv/datasets', # Path to the parent directory of the image folder
+    'data_path': '../dl4cv/datasets',  # Path to the parent directory of the image folder
 
-    'model_path': '/home/felipe/Projects/dl4cv/saves/train20190513154226/model40',
+    'model_path': '../dl4cv/saves/train20190513154226/model40',
 
     'batch_size': 100,
     'num_show_images': 20,              # Number of images to show
 }
+
 
 def eval_model(model, images):
     plt.interactive(False)
     for image in images:
         to_pil = transforms.ToPILImage()
 
-        plt.subplot(1,3,1)
+        plt.subplot(1, 3, 1)
         plt.imshow(to_pil(image), cmap='gray')
 
         restored = model(torch.unsqueeze(image, 0))
 
-        plt.subplot(1,3,2)
+        plt.subplot(1, 3, 2)
         plt.imshow(to_pil(restored[0]), cmap='gray')
 
         plt.subplot(1, 3, 3)
         plt.imshow(to_pil(abs(restored[0]-image)), cmap='gray')
         plt.show(block=True)
 
-data_set = datasets.ImageFolder(config['data_path'], transform=transforms.Compose([transforms.Grayscale(),
-                                                                                   transforms.ToTensor(),
-                                                                                   transforms.Normalize(mean=[0.5],std=[0.5])]))
 
-data_loader   = torch.utils.data.DataLoader(dataset=data_set, batch_size=config['batch_size'])
+data_set = datasets.ImageFolder(
+    config['data_path'],
+    transform=transforms.Compose([
+        transforms.Grayscale(),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.5], std=[0.5])
+    ])
+)
+
+data_loader = torch.utils.data.DataLoader(dataset=data_set, batch_size=config['batch_size'])
 
 model = torch.load(config['model_path'])
 
 batch = next(iter(data_loader))
 batch = batch[0]
+images = batch[np.linspace(0, config['batch_size']-1, config['num_show_images'], dtype=int).tolist()]
 
-eval_model(model, batch[np.linspace(0, config['batch_size']-1, config['num_show_images'], dtype=int).tolist()])
+eval_model(model, images)
--- dl4cv/solver.py
+++ dl4cv/solver.py
@@ -88,9 +88,10 @@ class Solver(object):
                 train_loss_avg = (smooth_window_train-1)/smooth_window_train*train_loss_avg + 1/smooth_window_train*loss.item()
 
                 if log_after_iters is not None and (i_iter % log_after_iters == 0):
-                    print("Iteration " + str(i_iter) + "/" + str(n_iters) + "   Train loss: " +
-                          "{0:.6f}".format(loss.item()) + "   Avg: " + "{0:.6f}".format(train_loss_avg) + " - " +
-                          str(int((time.time()-t_start_iter)*1000)) + "ms")
+                    print("Iteration " + str(i_iter) + "/" + str(n_iters) +
+                          "   Train loss: " + "{0:.6f}".format(loss.item()) +
+                          "   Avg: " + "{0:.6f}".format(train_loss_avg) +
+                          " - " + str(int((time.time()-t_start_iter)*1000)) + "ms")
 
             # Validate model
             print("\nValidate model after epoch " + str(i_epoch+1) + '/' + str(num_epochs))
@@ -115,8 +116,9 @@ class Solver(object):
             val_loss /= num_val_batches
             self.history['val_loss_history'].append(val_loss)
 
-            print('Avg Train Loss: ' + "{0:.6f}".format(
-                train_loss_avg) + '   Val loss: ' + "{0:.6f}".format(val_loss) + "   - " + str(int((time.time() - t_start_epoch) * 1000)) + "ms\n")
+            print('Avg Train Loss: ' + "{0:.6f}".format(train_loss_avg) +
+                  '   Val loss: ' + "{0:.6f}".format(val_loss) +
+                  "   - " + str(int((time.time() - t_start_epoch) * 1000)) + "ms\n")
 
             # Save model and solver
             if save_after_epochs is not None and ((i_epoch + 1) % save_after_epochs == 0):
