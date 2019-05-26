ae0aeacb4a70347661e655a7d60666695cac5222
Felix Meissen <felix.meissen@gmail.com>
2019-05-26 10:11:52 +0200

    New dataset for explicit disentanglement

--- dl4cv/dataset_utils.py
+++ dl4cv/dataset_utils.py
@@ -4,6 +4,7 @@ import torch
 from torch.utils.data.dataset import Dataset
 from torchvision.datasets.folder import \
     pil_loader, has_file_allowed_extension, IMG_EXTENSIONS
+from torchvision import transforms
 
 from dl4cv.utils import read_csv
 
@@ -112,3 +113,71 @@ class CustomDatasetRAM(Dataset):
         y = seq['images'][-1]
 
         return x, y, meta
+
+
+class ForwardAndMirroredDatasetRAM(Dataset):
+    """
+    Dataset which outputs a sequence once normal and once horizontally flipped.
+    Loads all data to RAM upon init, only loads num_sequences.
+    """
+
+    def __init__(self, path, num_sequences, load_meta=False):
+        self.transform_normal = transforms.Compose([
+            transforms.Grayscale(),
+            transforms.ToTensor()
+        ])
+        self.transform_mirrored = transforms.Compose([
+            transforms.Grayscale(),
+            transforms.RandomHorizontalFlip(p=1.0),
+            transforms.ToTensor()
+        ])
+        self.load_meta = load_meta
+        self.sequences = []
+        i_sequences = 0
+
+        for root, dir_names, _ in sorted(os.walk(path)):
+            for dir_name in sorted(dir_names, key=lambda s: int(s.split("seq")[1])):
+                if i_sequences == num_sequences:
+                    break
+
+                sequence = {'images': [], 'meta': []}
+                seq_path = os.path.join(root, dir_name)
+
+                if self.load_meta:
+                    meta_path = os.path.join(seq_path, 'meta.csv')
+                    meta = read_csv(meta_path)
+                    sequence['meta'] = meta
+
+                for _, _, fnames in os.walk(seq_path):
+                    fnames = [fname for fname in fnames if fname != 'meta.csv']
+
+                    for fname in sorted(fnames, key=lambda s: int(s.split("frame")[1].split(".")[0])):
+                        if has_file_allowed_extension(fname, IMG_EXTENSIONS):
+                            img_path = os.path.join(seq_path, fname)
+                            img = pil_loader(img_path)
+                            sequence['images'].append(img)
+
+                self.sequences.append(sequence)
+                i_sequences += 1
+
+        assert i_sequences == num_sequences
+
+    def __len__(self):
+        return len(self.sequences)
+
+    def __getitem__(self, index):
+        seq = self.sequences[index]
+
+        if self.load_meta:
+            meta = seq['meta']
+        else:
+            meta = 0
+
+        imgs_normal = [self.transform_normal(img) for img in seq['images']]
+        imgs_mirrored = [self.transform_mirrored(img) for img in seq['images']]
+
+        imgs_normal = torch.cat(imgs_normal)
+
+        imgs_mirrored = torch.cat(imgs_mirrored)
+
+        return imgs_normal, imgs_mirrored, meta
