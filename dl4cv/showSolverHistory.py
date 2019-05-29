58683b9d7fbfaa342a4b345cd0a940e999dcbc72
Felipe <felipepeter@web.de>
2019-05-29 15:44:27 +0200

    showSolverHistory also shows KL-Divergence

--- dl4cv/showSolverHistory.py
+++ dl4cv/showSolverHistory.py
@@ -2,30 +2,33 @@ import matplotlib.pyplot as plt
 import numpy as np
 from dl4cv.solver import Solver
 
-path = '../saves/train20190521135315/solver1'
+path = '../saves/train20190529153044/solver10'
 
 
 solver = Solver()
 solver.load(path, only_history=True)
 
-print("Stop reason: %s" %solver.stop_reason)
-print("Stop time: %fs" %solver.training_time_s)
+print("Stop reason: %s" % solver.stop_reason)
+print("Stop time: %fs" % solver.training_time_s)
 
-train_loss = np.array(solver.history['train_loss_history'])
-val_loss = np.array(solver.history['val_loss_history'])
+train_loss = np.array(solver.history['train_loss'])
+val_loss = np.array(solver.history['val_loss'])
+kl_divergence  = np.array(solver.history['kl_divergence'])
+reconstruction_loss = np.array(solver.history['reconstruction_loss'])
 
 f, (ax1, ax2, ax3) = plt.subplots(3,1)
 
 ax1.plot(train_loss)
+ax1.plot(np.linspace(1, len(train_loss), len(val_loss)), val_loss)
 ax1.set_xlabel("Iterations")
-ax1.set_ylabel("Train loss")
+ax1.set_ylabel("Train/Val loss")
 
-ax2.plot(val_loss)
-ax2.set_xlabel("Epoch")
-ax2.set_ylabel("Validation loss")
+ax2.plot(kl_divergence)
+ax2.set_xlabel("Iterations")
+ax2.set_ylabel("KL Divergence")
 
-ax3.plot(train_loss)
-ax3.plot(val_loss)
-# ax3.plot(np.arange(0,len(val_loss))*460,np.convolve(val_loss, np.ones((15,)) / 15, mode='valid'))
+ax3.plot(reconstruction_loss)
+ax3.set_xlabel("Iterations")
+ax3.set_ylabel("Reconstruction Loss")
 
 plt.show()
