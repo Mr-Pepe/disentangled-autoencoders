826f2b129f9a12a468a20c69448b3ecfa85166dc
Felix Meissen <felix.meissen@gmail.com>
2019-05-29 13:35:17 +0200

    More generic version of showModelOutput

--- dl4cv/showModelOutput.py
+++ dl4cv/showModelOutput.py
@@ -11,8 +11,10 @@ config = {
     'show_images': True,
 
     'data_path': '../datasets/ball',  # Path to directory of the image folder
+    'len_inp_sequence': 3,
+    'len_out_sequence': 3,
 
-    'model_path': '../saves/train20190522122921/model170',
+    'model_path': '../saves/train20190529113101/model100',
 
     'batch_size': 1000,
     'num_show_images': 10,              # Number of images to show
@@ -22,7 +24,7 @@ config = {
 def eval_model(model, samples):
     plt.interactive(False)
 
-    num_images = sequence_length+2
+    num_images = sequence_length + 2 * out_length
 
     all_z = torch.Tensor()
 
@@ -30,33 +32,42 @@ def eval_model(model, samples):
 
         x, y, meta = sample
 
+        print(x.shape, y.shape)
+
         y_pred, latent_stuff = model(torch.unsqueeze(x, 0))
 
         if config['show_images']:
 
             to_pil = transforms.ToPILImage()
 
-            for i in range(sequence_length-1):
-                ax = plt.subplot(2, num_images/2, i+1)
+            for i in range(inp_length):
+                ax = plt.subplot(3, num_images/3, i + 1)
                 plt.imshow(to_pil(x[i]), cmap='gray')
-                ax.set_title('frame t{}'.format(-(sequence_length-2) + i))
+                ax.set_title('frame t{}'.format(-(sequence_length - 2) + i))
 
-            ax = plt.subplot(2, num_images/2, sequence_length)
-            plt.imshow(to_pil(y), cmap='gray')
-            ax.set_title('Ground truth t+1')
+            for i in range(inp_length, 3 * out_length, 3):
+                # Ground Truth image
+                ax = plt.subplot(3, num_images/3, i + 1)
+                plt.imshow(to_pil(y[i - inp_length]), cmap='gray')
+                ax.set_title('Ground truth t+{}'.format(i + 1 - inp_length))
 
-            ax = plt.subplot(2, num_images/2, sequence_length+1)
-            plt.imshow(to_pil(y_pred[0]), cmap='gray')
-            ax.set_title('Prediction t+1')
+                # Predicted image
+                ax = plt.subplot(3, num_images / 3, i + 2)
+                plt.imshow(to_pil(y_pred[i - inp_length]), cmap='gray')
+                ax.set_title('Prediction t+{}'.format(i + 1 - inp_length))
 
-            ax = plt.subplot(2, num_images/2, sequence_length+2)
-            plt.imshow(to_pil(abs(y_pred[0]-y)), cmap='gray')
-            ax.set_title('Deviation')
+                # Deviation
+                ax = plt.subplot(3, num_images / 3, i + 3)
+                diff = abs(y_pred[i - inp_length] - y[i - inp_length])
+                plt.imshow(to_pil(diff), cmap='gray')
+                ax.set_title('Deviation t+{}'.format(i + 1 - inp_length))
 
             plt.show(block=True)
 
 
-sequence_length = 4  # 3 images as input sequence, 1 predicted image
+inp_length = config['len_inp_sequence']
+out_length = config['len_out_sequence']
+sequence_length = inp_length + out_length
 
 dataset = CustomDataset(
     config['data_path'],
@@ -64,7 +75,9 @@ dataset = CustomDataset(
         transforms.Grayscale(),
         transforms.ToTensor()
     ]),
-    sequence_length=sequence_length
+    sequence_length=sequence_length,
+    len_inp_sequence=config['len_inp_sequence'],
+    len_out_sequence=config['len_out_sequence']
 )
 
 data_loader = torch.utils.data.DataLoader(
