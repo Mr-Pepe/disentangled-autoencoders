eea2fc1f77c65bdccdeeab4c02858dad8ebdf645
Felipe <felipepeter@web.de>
2019-05-29 15:21:39 +0200

    Adjusted showModelOutput for sequence outputs

--- dl4cv/showModelOutput.py
+++ dl4cv/showModelOutput.py
@@ -3,7 +3,7 @@ import numpy as np
 import torch
 import torchvision.transforms as transforms
 
-from dl4cv.dataset_utils import CustomDataset
+from dl4cv.dataset_utils import CustomDataset, CustomDatasetRAM
 from torch.utils.data.sampler import SequentialSampler
 
 
@@ -14,7 +14,7 @@ config = {
     'len_inp_sequence': 3,
     'len_out_sequence': 3,
 
-    'model_path': '../saves/train20190529113101/model100',
+    'model_path': '../saves/train20190529141500/model10',
 
     'batch_size': 1000,
     'num_show_images': 10,              # Number of images to show
@@ -24,15 +24,14 @@ config = {
 def eval_model(model, samples):
     plt.interactive(False)
 
-    num_images = sequence_length + 2 * out_length
+    num_cols = max(config['len_inp_sequence'], config['len_out_sequence'])
+    num_rows = 4
 
-    all_z = torch.Tensor()
+    plt.rcParams.update({'font.size': 8})
 
     for sample in samples:
 
         x, y, meta = sample
-
-        print(x.shape, y.shape)
 
         y_pred, latent_stuff = model(torch.unsqueeze(x, 0))
 
@@ -40,27 +39,42 @@ def eval_model(model, samples):
 
             to_pil = transforms.ToPILImage()
 
-            for i in range(inp_length):
-                ax = plt.subplot(3, num_images/3, i + 1)
-                plt.imshow(to_pil(x[i]), cmap='gray')
-                ax.set_title('frame t{}'.format(-(sequence_length - 2) + i))
+            f, axes = plt.subplots(num_rows, num_cols)
 
-            for i in range(inp_length, 3 * out_length, 3):
-                # Ground Truth image
-                ax = plt.subplot(3, num_images/3, i + 1)
-                plt.imshow(to_pil(y[i - inp_length]), cmap='gray')
-                ax.set_title('Ground truth t+{}'.format(i + 1 - inp_length))
+            # Plot input sequence
+            for i in range(config['len_inp_sequence']):
+                axes[0, i].imshow(to_pil(x[i]), cmap='gray')
+                # axes[0, i].set_title('frame t{}'.format(-(sequence_length - 1) + i))
+                # plt.show()
 
-                # Predicted image
-                ax = plt.subplot(3, num_images / 3, i + 2)
-                plt.imshow(to_pil(y_pred[i - inp_length]), cmap='gray')
-                ax.set_title('Prediction t+{}'.format(i + 1 - inp_length))
+            for i in range(config['len_out_sequence']):
+                # Plot ground truth
+                axes[1, i].imshow(to_pil(y[i]), cmap='gray')
+                # axes[1, i].set_title('Ground truth t+{}'.format(i + 1 - inp_length))
 
-                # Deviation
-                ax = plt.subplot(3, num_images / 3, i + 3)
-                diff = abs(y_pred[i - inp_length] - y[i - inp_length])
-                plt.imshow(to_pil(diff), cmap='gray')
-                ax.set_title('Deviation t+{}'.format(i + 1 - inp_length))
+                # Plot prediction
+                axes[2, i].imshow(to_pil(y_pred[0, i]), cmap='gray')
+                # axes[2, i].set_title('Prediction t+{}'.format(i + 1 - inp_length))
+
+                # Plot Deviation
+                diff = abs(y_pred[0, i] - y[i])
+                axes[3, i].imshow(to_pil(diff), cmap='gray')
+                # axes[3, i].set_title('Deviation t+{}'.format(i + 1 - inp_length))
+
+            # Remove axis ticks
+            for ax in axes.reshape(-1):
+                ax.axis('off')
+
+            # Label rows
+            labels = {0: 'Input sequence',
+                      1: 'Ground truth',
+                      2: 'Prediction',
+                      3: 'Deviation'}
+
+            for i in range(num_rows):
+                plt.sca(axes[i, 0])
+                plt.tick_params(labelleft=True)
+                axes[i, 0].set_ylabel(labels[i])
 
             plt.show(block=True)
 
@@ -69,13 +83,12 @@ inp_length = config['len_inp_sequence']
 out_length = config['len_out_sequence']
 sequence_length = inp_length + out_length
 
-dataset = CustomDataset(
+dataset = CustomDatasetRAM(
     config['data_path'],
     transform=transforms.Compose([
         transforms.Grayscale(),
         transforms.ToTensor()
     ]),
-    sequence_length=sequence_length,
     len_inp_sequence=config['len_inp_sequence'],
     len_out_sequence=config['len_out_sequence']
 )
