86a5f2e6abad9e51651b81aa004306ab05992bed
Felix Meissen <felix.meissen@gmail.com>
2019-06-11 09:11:15 +0200

    Extend setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,11 @@ from setuptools import setup
 setup(
    name='dl4cv',
    version='1.0',
-   packages=['dl4cv', 'dl4cv.models'],
+   packages=[
+      'dl4cv',
+      'dl4cv.models',
+      'dl4cv.dataset_stuff',
+      'dl4cv.eval',
+      'dl4cv.experimental'
+   ],
 )
