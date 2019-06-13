01e0e88dda376260cdd8ce4e768236df4b4c20d2
Felipe <felipepeter@web.de>
2019-06-13 14:20:43 +0200

    Also ignore subdirectories of saves during package search

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup, find_packages
 setup(
    name='dl4cv',
    version='1.0',
-   packages=find_packages(exclude=('datasets', 'datasets.*', 'saves'))
+   packages=find_packages(exclude=('datasets', 'datasets.*', 'saves', 'saves.*'))
 )
