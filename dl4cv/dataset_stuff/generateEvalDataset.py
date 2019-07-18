56add0e23d9aeae305455e8ac8ddbe0069dc612b
Felix Meissen <felix.meissen@gmail.com>
2019-07-18 10:46:03 +0200

    Generate evaluation dataset where one latent variable is constant for each subset

--- dl4cv/dataset_stuff/generateEvalDataset.py
+++ dl4cv/dataset_stuff/generateEvalDataset.py
@@ -1,194 +1,234 @@
 import os
+import pickle
 
-import math
+from dl4cv.utils import Config
 import numpy as np
 import torch
 from PIL import Image, ImageDraw
+from dl4cv.eval.eval_functions import analyze_dataset
 
-from dl4cv.utils import save_csv
+config = Config({
+    'save_dir_path': '../../../datasets/evalDataset',
+    'num_sequences_per_latent': 100,
+    'sequence_length': 5,
+    'window_size_x': 32,
+    'window_size_y': 32,
+    'ball_radius': 2,
+    't_frame': 1 / 30,
+    'eval_before_generating': False,  # Evaluate the dataset before generating it
+    'generate': True                # Generate the dataset
+})
 
-dataset_path = "../../../datasets/evalDataset"
+# Define trajectory properties. Do this separately as some values need the config to already exist
+config.update({
+    'avoid_collisions': False,
+    'sample_mode': 'x_start, v_start, a_start',  # modes: 'x_start, v_start, a_start',
+                                                 #        'only_position'
 
-# make dataset_path absolute
+    'x_min': config.ball_radius,
+    'x_max': config.window_size_x - config.ball_radius,
+    'y_min': config.ball_radius,
+    'y_max': config.window_size_y - config.ball_radius,
+
+    'x_mean': config.window_size_x / 2,
+    'x_std':  config.window_size_x / 1,
+
+    'y_mean': config.window_size_y / 2,
+    'y_std':  config.window_size_y / 1,
+
+    'vx_std': 10,
+    'vy_std': 10,
+
+    'ax_std': 10,
+    'ay_std': 10
+})
+
+
+# make save_dir_path absolute
 file_dir = os.path.dirname(os.path.realpath(__file__))
-dataset_path = os.path.join(file_dir, dataset_path)
-
-os.makedirs(dataset_path, exist_ok=True)
+config['save_dir_path'] = os.path.join(file_dir, config['save_dir_path'])
 
 
-SEQUENCE_LENGTH = 50
+def equation_of_motion(x_0, y_0, vx_0, vy_0, ax, ay, t):
+    x_t = x_0.view(-1, 1) + vx_0.view(-1, 1) * t + 0.5 * ax.view(-1, 1) * t * t
+    y_t = y_0.view(-1, 1) + vy_0.view(-1, 1) * t + 0.5 * ay.view(-1, 1) * t * t
 
-T_FRAME = 1/30
-WINDOW_SIZE_X = 32
-WINDOW_SIZE_Y = 32
-BALL_RADIUS = 3
-V_MAX = 300
+    vx_t = vx_0.view(-1, 1) + ax.view(-1, 1) * t
+    vy_t = vy_0.view(-1, 1) + ay.view(-1, 1) * t
 
-"""Generate the variations in every dimension"""
-
-# Variation in pos_x
-pos_x_start = BALL_RADIUS
-pos_x_end = WINDOW_SIZE_X - BALL_RADIUS
-pos_x_num_samples = WINDOW_SIZE_X - 2 * BALL_RADIUS + 1
-
-# Variation in pos_y
-pos_y_start = BALL_RADIUS
-pos_y_end = WINDOW_SIZE_Y - BALL_RADIUS
-pos_y_num_samples = WINDOW_SIZE_Y - 2 * BALL_RADIUS + 1
+    return x_t, y_t, vx_t, vy_t
 
 
-std_vel = 15
+def get_collisions(x, y, x_min, x_max, y_min, y_max):
+    collisions = np.zeros((x.shape[0]))
 
-# Variation in vel_x
-vel_x_start = - 3 * std_vel
-vel_x_end = 3 * std_vel
-vel_x_num_samples = (vel_x_end - vel_x_start) // 3 + 1
+    eps = 1e-8
 
-# Variation in vel_y
-vel_y_start = - 3 * std_vel
-vel_y_end = 3 * std_vel
-vel_y_num_samples = (vel_y_end - vel_y_start) // 3 + 1
+    collisions[(x_min - x.min(dim=1).values > eps).nonzero()] = 1
+    collisions[(x.max(dim=1).values - x_max > eps).nonzero()] = 1
+    collisions[(y_min - y.min(dim=1).values > eps).nonzero()] = 1
+    collisions[(y.max(dim=1).values - y_max > eps).nonzero()] = 1
 
-
-std_acc = 10
-
-# Variation in acc_x
-acc_x_start = - 3 * std_acc
-acc_x_end = 3 * std_acc
-acc_x_num_samples = (acc_x_end - acc_x_start) // 2 + 1
-
-# Variation in acc_y
-acc_y_start = - 3 * std_acc
-acc_y_end = 3 * std_acc
-acc_y_num_samples = (acc_y_end - acc_y_start) // 2 + 1
-
-# Velocity and acceleration must be inverted because the frames are saved
-# in the reverse order so the velocity at frame[-1] is actually -vel[0]
-variables = {
-    'pos_x': np.linspace(pos_x_start, pos_x_end, pos_x_num_samples),
-    'pos_y': np.linspace(pos_y_start, pos_y_end, pos_y_num_samples),
-    'vel_x': np.linspace(vel_x_start, vel_x_end, vel_x_num_samples),
-    'vel_y': np.linspace(vel_y_start, vel_y_end, vel_y_num_samples),
-    'acc_x': np.linspace(acc_x_start, acc_x_end, acc_x_num_samples),
-    'acc_y': np.linspace(acc_y_start, acc_y_end, acc_y_num_samples)
-}
-
-std = {
-    'pos_x': WINDOW_SIZE_X / 4,
-    'pos_y': WINDOW_SIZE_Y / 4,
-    'vel_x': std_vel,
-    'vel_y': std_vel,
-    'acc_x': std_acc,
-    'acc_y': std_acc
-}
+    return collisions
 
 
-def get_new_state(x, y, vx, vy, ax, ay, x_min, x_max, y_min, y_max, t_frame):
-    vx_new = vx + ax * t_frame
-    vy_new = vy + ay * t_frame
-
-    x_new = x + vx * t_frame + 0.5 * ax * t_frame * t_frame
-    y_new = y + vy * t_frame + 0.5 * ay * t_frame * t_frame
-
-    if x_new < x_min:
-        x_new = x_min + (x_min - x_new)
-        vx_new = -vx_new
-    if x_new > x_max:
-        x_new = x_max - (x_new - x_max)
-        vx_new = -vx_new
-
-    if y_new < y_min:
-        y_new = y_min + (y_min - y_new)
-        vy_new = -vy_new
-    if y_new > y_max:
-        y_new = y_max - (y_new - y_max)
-        vy_new = -vy_new
-
-    return x_new, y_new, vx_new, vy_new
-
-
-def draw_ball(screen, x, y):
+def draw_ball(screen, window_size_x, window_size_y, ball_radius, x, y):
+    # Reset to a black image
     screen.rectangle(
-        xy=[(0, 0), (WINDOW_SIZE_X, WINDOW_SIZE_Y)],
+        xy=[(0, 0), (window_size_x, window_size_y)],
         fill='black',
         outline=None
     )
+    # Draw the ball on the clean image
     screen.ellipse(
-        xy=[(x - BALL_RADIUS, y - BALL_RADIUS), (x + BALL_RADIUS, y + BALL_RADIUS)],
+        xy=[(x - ball_radius, y - ball_radius), (x + ball_radius, y + ball_radius)],
         width=0,
         fill='white'
     )
 
 
-# create image to draw on
-img = Image.new(mode="L", size=(WINDOW_SIZE_X, WINDOW_SIZE_Y))
-# create screen
-screen = ImageDraw.Draw(img)
+def get_initial_velocities(x_start, x_end, y_start, y_end, ax, ay, t_end):
+    vx_0 = (x_end - x_start - 0.5 * ax * t_end * t_end) / t_end
+    vy_0 = (y_end - y_start - 0.5 * ay * t_end * t_end) / t_end
+
+    return vx_0, vy_0
 
 
-x_max = WINDOW_SIZE_X - BALL_RADIUS
-x_min = BALL_RADIUS
-y_max = WINDOW_SIZE_Y - BALL_RADIUS
-y_min = BALL_RADIUS
+def get_trajectories(x_start, y_start, vx_start, vy_start, ax, ay, t_frame, sequence_length):
+    t = torch.arange(sequence_length).float() * t_frame
+    t = torch.ones(x_start.shape[0], 1) * t.view(1, -1)
+
+    return equation_of_motion(x_start, y_start, vx_start, vy_start, ax, ay, t)
 
 
-for key in variables:
-    # Create a mini dataset for each varying variable
-    path = os.path.join(dataset_path, key)
-    os.makedirs(path, exist_ok=True)
+def generate_data(c):
 
-    num_sequences = len(variables[key])
-    print("Generating %d sequences for %s" % (num_sequences, key))
+    # x_mean = config.update
 
-    # Save varying variable
-    save_csv(variables[key], os.path.join(path, 'linspace.csv'))
+    if config['sample_mode'] == 'x_start, v_start, a_start':
+        latent_names = ['px', 'py', 'vx', 'vy', 'ax', 'ay']
+        latent_means = [c.x_mean, c.y_mean, 0, 0, 0, 0]
+    elif config['sample_mode'] == 'only_position':
+        latent_names = ['px', 'py']
+        latent_means = [c.x_mean, c.y_mean]
+    else:
+        raise NotImplementedError("Invalid sample_mode: {}".format(config['sample_mode']))
 
-    # Set values for the params of the current mini dataset
-    inner_vars = {}
-    for inner_key in variables:
-        if key == inner_key:
-            inner_vars[inner_key] = variables[key]
-        else:
-            if inner_key == 'pos_x':
-                inner_vars[inner_key] = [torch.normal(
-                    mean=WINDOW_SIZE_X / 2,
-                    std=torch.ones(1) * std[inner_key]
-                ).item()] * num_sequences
-            elif inner_key == 'pos_y':
-                inner_vars[inner_key] = [torch.normal(
-                    mean=WINDOW_SIZE_Y / 2,
-                    std=torch.ones(1) * std[inner_key]
-                ).item()] * num_sequences
+    for i_latent in range(len(latent_names)):
+        print("Generating subset where {} is constant".format(latent_names[i_latent]))
+
+        latent_path = os.path.join(c.save_dir_path, latent_names[i_latent])
+
+        os.makedirs(latent_path, exist_ok=True)
+
+        t_end = (c.sequence_length - 1) * c.t_frame
+
+        # create image to draw on
+        img = Image.new(mode="L", size=(c.window_size_x, c.window_size_y))
+        # create screen
+        screen = ImageDraw.Draw(img)
+
+        # Initialize x,y,vx,vy,ax,ay
+        start_vars = [torch.zeros((c.num_sequences_per_latent,)) for _ in range(6)]
+
+        collisions = np.ones((c.num_sequences_per_latent))
+
+        # Generate Trajectories for all the sequences
+        while collisions.any():
+            idx = collisions.nonzero()[0]
+
+            if config['sample_mode'] == 'x_start, v_start, a_start':
+
+                start_vars[0][idx] = torch.rand_like(torch.Tensor(idx))*(c.x_max - c.x_min) + c.x_min
+                start_vars[1][idx] = torch.rand_like(torch.Tensor(idx))*(c.y_max - c.y_min) + c.y_min
+
+                start_vars[2][idx] = torch.normal(0, torch.ones([idx.shape[0]]) * c.vx_std)
+                start_vars[3][idx] = torch.normal(0, torch.ones([idx.shape[0]]) * c.vy_std)
+
+                start_vars[4][idx] = torch.normal(0, torch.ones([idx.shape[0]]) * c.ax_std)
+                start_vars[5][idx] = torch.normal(0, torch.ones([idx.shape[0]]) * c.ay_std)
+
+                # Always keep one latent variable constant
+                start_vars[i_latent][idx] = torch.ones_like(start_vars[i_latent]) * latent_means[i_latent]
+
+                x, y, vx, vy = get_trajectories(*start_vars, c.t_frame, c.sequence_length)
+
+            elif config['sample_mode'] == 'only_position':
+                start_vars[0][idx] = torch.normal(c.x_mean, torch.ones([idx.shape[0]]) * c.x_std)
+                start_vars[1][idx] = torch.normal(c.y_mean, torch.ones([idx.shape[0]]) * c.y_std)
+
+                # Always keep one latent variable constant
+                start_vars[i_latent][idx] = torch.ones_like(start_vars[i_latent]) * latent_means[i_latent]
+
+                x, y, vx, vy = get_trajectories(*start_vars, c.t_frame, c.sequence_length)
+
             else:
-                inner_vars[inner_key] = [torch.normal(
-                    mean=0,
-                    std=torch.ones(1) * std[inner_key]
-                ).item()] * num_sequences
+                raise Exception("Invalid sample_mode: {}".format(config['sample_mode']))
 
-    # Create all n sequences of a mini dataset
-    for i_seq in range(num_sequences):
-        sequence_path = os.path.join(path, "seq%d" % i_seq)
-        os.makedirs(sequence_path, exist_ok=True)
+            collisions = get_collisions(x, y, c.x_min, c.x_max, c.y_min, c.y_max)
 
-        x = min(x_max, max(x_min, inner_vars['pos_x'][i_seq]))
-        y = min(y_max, max(y_min, inner_vars['pos_y'][i_seq]))
-        vx = inner_vars['vel_x'][i_seq]
-        vy = inner_vars['vel_y'][i_seq]
-        ax = inner_vars['acc_x'][i_seq]
-        ay = inner_vars['acc_y'][i_seq]
+            if c.avoid_collisions:
+                print("{} collisions of {} sequences".format(collisions.sum(), c.num_sequences))
+            else:
+                print("No collision avoidance... but we had {} in {} sequences".format(
+                    collisions.sum(), c.num_sequences_per_latent)
+                )
+                collisions = np.zeros((c.num_sequences_per_latent))
 
-        # Create all frames of a sequence
-        for i_frame in range(SEQUENCE_LENGTH):
+        if config['eval_before_generating']:
+            trajectories = np.zeros((c.num_sequences_per_latent, c.sequence_length, 6))
 
-            frame_path = os.path.join(
-                sequence_path, 'frame%d.jpeg' % (i_frame + 1)
-            )
+            trajectories[:, :, 0] = x
+            trajectories[:, :, 1] = y
+            trajectories[:, :, 2] = vx
+            trajectories[:, :, 3] = vy
+            trajectories[:, :, 4] = np.repeat(start_vars[4].reshape(-1, 1), c.sequence_length, 1)
+            trajectories[:, :, 5] = np.repeat(start_vars[5].reshape(-1, 1), c.sequence_length, 1)
 
-            draw_ball(screen, x, y)
-            img.save(frame_path)
+            analyze_dataset(trajectories, mode='points')
 
-            # Limit velocities to V_MAX
-            vx = math.copysign(V_MAX, vx) if abs(vx) > V_MAX else vx
-            vy = math.copysign(V_MAX, vy) if abs(vy) > V_MAX else vy
+        if config['generate']:
+            # Save configuration
+            with open(os.path.join(c.save_dir_path, 'config.p'), 'wb') as f:
+                pickle.dump(c, f)
 
-            x, y, vx, vy = get_new_state(x, y, vx, vy, ax, ay, x_min, x_max, y_min, y_max, T_FRAME)
+            # Generate frames for the sequences
+            for i_sequence in range(c.num_sequences_per_latent):
+
+                save_path_sequence = os.path.join(
+                    latent_path,
+                    'seq' + str(i_sequence)
+                )
+
+                os.makedirs(save_path_sequence, exist_ok=True)
+
+                ground_truth = np.zeros((c.sequence_length, 6))
+
+                for i_frame in range(c.sequence_length):
+
+                    save_path_frame = os.path.join(
+                        save_path_sequence,
+                        'frame' + str(i_frame) + '.jpeg'
+                    )
+
+                    ground_truth[i_frame] = np.array([x[i_sequence, i_frame], y[i_sequence, i_frame],
+                                                      vx[i_sequence, i_frame], vy[i_sequence, i_frame],
+                                                      start_vars[4][i_sequence], start_vars[5][i_sequence]])
+
+                    draw_ball(screen, c.window_size_x, c.window_size_y, c.ball_radius, x[i_sequence, i_frame], y[i_sequence, i_frame])
+                    img.save(save_path_frame)
+
+                # Save the values at the last
+                save_path_ground_truth = os.path.join(
+                    save_path_sequence,
+                    'ground_truth'
+                )
+                np.save(save_path_ground_truth, ground_truth)
+
+                if (i_sequence+1) % 100 == 0:
+                    print("Generated sequence: %d of %d with length %d ..." % (
+                        i_sequence+1, c.num_sequences_per_latent, c.sequence_length))
+
+
+if __name__ == '__main__':
+    generate_data(config)
