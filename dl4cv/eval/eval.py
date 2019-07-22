c8f7ce96a74ac5086a7f578954d60c2aa61022fc
Felipe <felipepeter@web.de>
2019-07-22 16:46:26 +0200

    Make train and eval callable

--- dl4cv/eval/eval.py
+++ dl4cv/eval/eval.py
@@ -25,172 +25,101 @@ from dl4cv.eval.eval_functions import \
 
 
 config = {
-    'analyze_dataset': False,            # Plot positions of the desired data-points
-    'show_solver_history': True,        # Plot losses of the training
-    'show_latent_variables': False,      # Show the latent variables for the desired data-points
-    'show_model_output': False,          # Show the model output for the desired data-points
+    'analyze_dataset': False,            # Plot positions of the desired datapoints
+    'show_solver_history': False,        # Plot losses of the training
+    'show_latent_variables': False,      # Show the latent variables for the desired datapoints
+    'show_model_output': True,          # Show the model output for the desired datapoints
     'eval_correlation': False,           # Plot the correlation between the latent variables and ground truth
-    'latent_variable_slideshow': False,  # Create a slide-show varying over all latent variables
-    'print_training_config': False,      # Print the config that was used for training the model
+    'latent_variable_slideshow': False,   # Create a slideshow varying over all latent variables
+    'print_training_config': False,       # Print the config that was used for training the model
     'latent_walk_gifs': False,
     'walk_over_question': False,
     'eval_disentanglement': False,       # Evaluate disentanglement according to the metric from the BetaVAE paper.
     'mutual_information_gap': False,     # Evaluate disentanglement according to the MIG score
 
-    'data_path': '../../../datasets/ball',  # Path to directory of the image folder
+    'data_path': '../../../datasets/ball_no_acc_x_y_different',  # Path to directory of the image folder
     'eval_data_path': '../../../datasets/evalDataset',
-    'len_inp_sequence': 0,
+    'len_inp_sequence': 5,
     'len_out_sequence': 1,
-    'num_samples': 500,                # Use the whole dataset if none for latent variables
+    'num_samples': 2000,                # Use the whole dataset if none for latent variables
     'num_show_images': 10,              # Number of outputs to show when show_model_output is True
 
-    'question': True,
+    'use_question': True,
 
-    'save_path': '../../saves/temp',  # Path to the directory where the model and solver are saved
+    'save_path': '../../saves/train20190719151645',  # Path to the directory where the model and solver are saved
     'epoch': None,                                  # Use last model and solver if epoch is none
 
     'use_cuda': False,
 }
 
 
-""" Configure evaluation with or without cuda """
-
-if config['use_cuda'] and torch.cuda.is_available():
-    device = torch.device("cuda")
-else:
-    device = torch.device("cpu")
-    torch.set_default_tensor_type('torch.FloatTensor')
 
 
-# make all paths absolute
-file_dir = os.path.dirname(os.path.realpath(__file__))
+def eval(config):
 
-config['data_path'] = os.path.join(file_dir, config['data_path'])
-config['eval_data_path'] = os.path.join(file_dir, config['eval_data_path'])
-config['save_path'] = os.path.join(file_dir, config['save_path'])
+    """ Configure evaluation with or without cuda """
 
-z = None
-mu = None
-
-
-def get_model_solver_paths(save_path, epoch):
-    print("Getting model and solver paths")
-    model_paths = []
-    solver_paths = []
-
-    for _, _, fnames in os.walk(save_path):
-        model_paths = [fname for fname in fnames if 'model' in fname]
-        solver_paths = [fname for fname in fnames if 'solver' in fname]
-
-    if not model_paths or not solver_paths:
-        raise Exception('Model or solver not found.')
-
-    if not epoch:
-        model_path = os.path.join(save_path, sorted(model_paths, key=lambda s: int(s.split("model")[1]))[-1])
-        solver_path = os.path.join(save_path, sorted(solver_paths, key=lambda s: int(s.split("solver")[1]))[-1])
+    if config['use_cuda'] and torch.cuda.is_available():
+        device = torch.device("cuda")
     else:
-        model_path = os.path.join(save_path, 'model' + str(epoch))
-        solver_path = os.path.join(save_path, 'solver' + str(epoch))
-
-    return model_path, solver_path
+        device = torch.device("cpu")
+        torch.set_default_tensor_type('torch.FloatTensor')
 
 
-print("Loading dataset")
+    # make all paths absolute
+    file_dir = os.path.dirname(os.path.realpath(__file__))
 
-dataset = CustomDataset(
-    config['data_path'],
-    transform=transforms.Compose([
-        transforms.Grayscale(),
-        transforms.ToTensor()
-    ]),
-    len_inp_sequence=config['len_inp_sequence'],
-    len_out_sequence=config['len_out_sequence'],
-    load_ground_truth=True,
-    question=config['question'],
-    load_to_ram=False,
-    load_config=True
-)
-dataset_config = pickle.load(open(os.path.join(config['data_path'], 'config.p'), 'rb'))
+    config['data_path'] = os.path.join(file_dir, config['data_path'])
+    config['eval_data_path'] = os.path.join(file_dir, config['eval_data_path'])
+    config['save_path'] = os.path.join(file_dir, config['save_path'])
 
-if config['num_samples'] is not None:
-    if config['num_show_images'] > len(dataset):
-        raise Exception('Dataset does not contain {} images to show'.format(config['num_show_images']))
-
-    # Sample equidistantly from dataset
-    indices = np.linspace(0, len(dataset) - 1, config['num_samples'], dtype=int).tolist()
-
-    dataset_list = [dataset[i] for i in indices]
-    ground_truth = [dataset.get_ground_truth(i) for i in indices]
-else:
-    dataset_list = dataset
-    ground_truth = [dataset.get_ground_truth(i) for i in range(len(dataset))]
-
-model_path, solver_path = get_model_solver_paths(config['save_path'], config['epoch'])
-
-print("Loading model and solver")
-solver = Solver()
-solver.load(solver_path, device=device, only_history=True)
-model = torch.load(model_path, map_location=device)
+    z = None
+    mu = None
 
 
-if config['analyze_dataset']:
-    print("Analysing dataset")
+    def get_model_solver_paths(save_path, epoch):
+        print("Getting model and solver paths")
+        model_paths = []
+        solver_paths = []
+
+        for _, _, fnames in os.walk(save_path):
+            model_paths = [fname for fname in fnames if 'model' in fname]
+            solver_paths = [fname for fname in fnames if 'solver' in fname]
+
+        if not model_paths or not solver_paths:
+            raise Exception('Model or solver not found.')
+
+        if not epoch:
+            model_path = os.path.join(save_path, sorted(model_paths, key=lambda s: int(s.split("model")[1]))[-1])
+            solver_path = os.path.join(save_path, sorted(solver_paths, key=lambda s: int(s.split("solver")[1]))[-1])
+        else:
+            model_path = os.path.join(save_path, 'model' + str(epoch))
+            solver_path = os.path.join(save_path, 'solver' + str(epoch))
+
+        return model_path, solver_path
+
+
+    print("Loading dataset")
+
+    dataset = CustomDataset(
+        config['data_path'],
+        transform=transforms.Compose([
+            transforms.Grayscale(),
+            transforms.ToTensor()
+        ]),
+        len_inp_sequence=config['len_inp_sequence'],
+        len_out_sequence=config['len_out_sequence'],
+        load_ground_truth=True,
+        question=config['use_question'],
+        load_to_ram=False,
+        load_config=True
+    )
+    dataset_config = pickle.load(open(os.path.join(config['data_path'], 'config.p'), 'rb'))
+
     if config['num_samples'] is not None:
-        indices = np.linspace(0, len(dataset) - 1, config['num_samples'], dtype=int).tolist()
-    else:
-        indices = range(len(dataset))
+        if config['num_show_images'] > len(dataset):
+            raise Exception('Dataset does not contain {} images to show'.format(config['num_show_images']))
 
-    trajectories = np.array([dataset.get_ground_truth(i) for i in indices])
-
-    analyze_dataset(
-        trajectories,
-        window_size_x=dataset_config['window_size_x'],
-        window_size_y=dataset_config['window_size_y'],
-        mode='points')
-
-
-if config['show_solver_history']:
-    print("Showing solver history")
-    show_solver_history(solver)
-
-
-if config['print_training_config']:
-    print_traning_config(solver)
-
-
-if config['show_latent_variables']:
-    print("Using {} samples to show latent variables".format(config['num_samples']))
-    z, mu = show_latent_variables(model, dataset_list)
-
-
-if config['show_model_output']:
-    print("Showing model output")
-    indices = np.linspace(0, len(dataset) - 1, config['num_show_images'], dtype=int).tolist()
-    show_model_output(model, [dataset[i] for i in indices], dataset.len_out_sequence)
-
-
-if config['eval_correlation']:
-    print("Evaluating correlation")
-
-    if z is None:
-        z, mu = show_latent_variables(model, dataset_list, show=False)
-
-    show_correlation(model, dataset_list, z, ground_truth)
-
-    if model.use_physics:
-        show_correlation_after_physics(model, dataset_list)
-    else:
-        print("Model without physics layer")
-
-
-if config['latent_variable_slideshow']:
-    print("Creating slideshow")
-    latent_variable_slideshow(model, dataset_list)
-
-
-if config['latent_walk_gifs']:
-    print("Creating GIFs for walks over latent variables")
-    if config['num_samples'] is not None:
         # Sample equidistantly from dataset
         indices = np.linspace(0, len(dataset) - 1, config['num_samples'], dtype=int).tolist()
 
@@ -200,41 +129,121 @@ if config['latent_walk_gifs']:
         dataset_list = dataset
         ground_truth = [dataset.get_ground_truth(i) for i in range(len(dataset))]
 
-    if mu is None:
-        z, mu = show_latent_variables(model, dataset_list, show=False)
+    model_path, solver_path = get_model_solver_paths(config['save_path'], config['epoch'])
 
-    show_latent_walk_gifs(model, mu, question=config['question'], len_out_sequence=dataset.len_out_sequence)
-
-if config['walk_over_question']:
-    print("Walk over questions")
-    walk_over_question(model, dataset)
-
-if config['eval_disentanglement']:
-    print("Evaluating disentanglement")
-    # load eval dataset to list
-    paths = [os.path.join(config['eval_data_path'], path) for path in dataset_config.latent_names]
-    eval_datasets = [
-        CustomDataset(
-            path,
-            transform=transforms.Compose([
-                transforms.Grayscale(),
-                transforms.ToTensor()
-            ]),
-            len_inp_sequence=config['len_inp_sequence'],
-            len_out_sequence=config['len_out_sequence'],
-            load_ground_truth=True,
-            question=config['question'],
-            load_to_ram=False,
-            load_config=False,
-        )
-        for path in paths]
-
-    train_accuracy = eval_disentanglement(model, eval_datasets, device, num_epochs=100)
-
-    print("Training set accuracy (metric for disentanglement): {:.4f}".format(train_accuracy))
+    print("Loading model and solver")
+    solver = Solver()
+    solver.load(solver_path, device=device, only_history=True)
+    model = torch.load(model_path, map_location=device)
+    model.eval()
 
 
-if config['mutual_information_gap']:
-    print("Computing mutual information gap")
-    mig = MIG(model, dataset, config['num_samples'], discrete=True)
-    print("MIG score: {} (ranges from 0 to 1 with 1=completely disentangled)".format(mig))
+    if config['analyze_dataset']:
+        print("Analysing dataset")
+        if config['num_samples'] is not None:
+            indices = np.linspace(0, len(dataset) - 1, config['num_samples'], dtype=int).tolist()
+        else:
+            indices = range(len(dataset))
+
+        trajectories = np.array([dataset.get_ground_truth(i) for i in indices])
+
+        analyze_dataset(
+            trajectories,
+            window_size_x=dataset_config['window_size_x'],
+            window_size_y=dataset_config['window_size_y'],
+            mode='points')
+
+
+    if config['show_solver_history']:
+        print("Showing solver history")
+        show_solver_history(solver)
+
+
+    if config['print_training_config']:
+        print_traning_config(solver)
+
+
+    if config['show_latent_variables']:
+        print("Using {} samples to show latent variables".format(config['num_samples']))
+        z, mu = show_latent_variables(model, dataset_list)
+
+
+    if config['show_model_output']:
+        print("Showing model output")
+        indices = np.linspace(0, len(dataset) - 1, config['num_show_images'], dtype=int).tolist()
+        show_model_output(model, dataset, indices, dataset.len_out_sequence)
+
+
+    if config['eval_correlation']:
+        print("Evaluating correlation")
+
+        if z is None:
+            z, mu = show_latent_variables(model, dataset_list, show=False)
+
+        show_correlation(model, dataset_list, z, ground_truth)
+
+        if model.use_physics:
+            show_correlation_after_physics(model, dataset_list)
+        else:
+            print("Model without physics layer")
+
+
+    if config['latent_variable_slideshow']:
+        print("Creating slideshow")
+        latent_variable_slideshow(model, dataset_list)
+
+
+    if config['latent_walk_gifs']:
+        print("Creating GIFs for walks over latent variables")
+        if config['num_samples'] is not None:
+            # Sample equidistantly from dataset
+            indices = np.linspace(0, len(dataset) - 1, config['num_samples'], dtype=int).tolist()
+
+            dataset_list = [dataset[i] for i in indices]
+            ground_truth = [dataset.get_ground_truth(i) for i in indices]
+        else:
+            dataset_list = dataset
+            ground_truth = [dataset.get_ground_truth(i) for i in range(len(dataset))]
+
+        if mu is None:
+            z, mu = show_latent_variables(model, dataset_list, show=False)
+
+        show_latent_walk_gifs(model, mu, question=config['use_question'], len_out_sequence=dataset.len_out_sequence)
+
+    if config['walk_over_question']:
+        print("Walk over questions")
+        walk_over_question(model, dataset)
+
+    if config['eval_disentanglement']:
+        print("Evaluating disentanglement")
+        # load eval dataset to list
+        paths = [os.path.join(config['eval_data_path'], path) for path in dataset_config.latent_names]
+        eval_datasets = [
+            CustomDataset(
+                path,
+                transform=transforms.Compose([
+                    transforms.Grayscale(),
+                    transforms.ToTensor()
+                ]),
+                len_inp_sequence=config['len_inp_sequence'],
+                len_out_sequence=config['len_out_sequence'],
+                load_ground_truth=True,
+                question=config['use_question'],
+                load_to_ram=False,
+                load_config=False,
+            )
+            for path in paths]
+
+        train_accuracy = eval_disentanglement(model, eval_datasets, device, num_epochs=100)
+
+        print("Training set accuracy (metric for disentanglement): {:.4f}".format(train_accuracy))
+
+
+    if config['mutual_information_gap']:
+        print("Computing mutual information gap")
+        mig = MIG(model, dataset, config['num_samples'], discrete=True)
+        print("MIG score: {} (ranges from 0 to 1 with 1=completely disentangled)".format(mig))
+
+
+if __name__ == '__main__':
+    eval(config)
--- dl4cv/train.py
+++ dl4cv/train.py
@@ -59,131 +59,123 @@ config = {
 }
 
 
-""" Make paths absolute """
+def train(config):
+    """ Make paths absolute """
 
-file_dir = os.path.dirname(os.path.realpath(__file__))
+    file_dir = os.path.dirname(os.path.realpath(__file__))
 
-config['model_path'] = os.path.join(file_dir, config['model_path'])
-config['solver_path'] = os.path.join(file_dir, config['solver_path'])
-config['data_path'] = os.path.join(file_dir, config['data_path'])
-config['save_path'] = os.path.join(file_dir, config['save_path'])
-config['tensorboard_log_dir'] = os.path.join(file_dir, config['tensorboard_log_dir'])
+    config['model_path'] = os.path.join(file_dir, config['model_path'])
+    config['solver_path'] = os.path.join(file_dir, config['solver_path'])
+    config['data_path'] = os.path.join(file_dir, config['data_path'])
+    config['save_path'] = os.path.join(file_dir, config['save_path'])
+    config['tensorboard_log_dir'] = os.path.join(file_dir, config['tensorboard_log_dir'])
 
+    """ Add a seed to have reproducible results """
 
-""" Add a seed to have reproducible results """
+    seed = 456
+    torch.manual_seed(seed)
 
-seed = 123
-torch.manual_seed(seed)
+    """ Configure training with or without cuda """
 
+    if config['use_cuda'] and torch.cuda.is_available():
+        device = torch.device("cuda")
+        torch.cuda.manual_seed(seed)
+        kwargs = {'pin_memory': True}
+        print("GPU available. Training on {}.".format(device))
+    else:
+        device = torch.device("cpu")
+        torch.set_default_tensor_type('torch.FloatTensor')
+        kwargs = {}
+        print("No GPU. Training on {}.".format(device))
 
-""" Configure training with or without cuda """
+    """ Load dataset """
 
-if config['use_cuda'] and torch.cuda.is_available():
-    device = torch.device("cuda")
-    torch.cuda.manual_seed(seed)
-    kwargs = {'pin_memory': True}
-    print("GPU available. Training on {}.".format(device))
-else:
-    device = torch.device("cpu")
-    torch.set_default_tensor_type('torch.FloatTensor')
-    kwargs = {}
-    print("No GPU. Training on {}.".format(device))
-
-
-""" Load dataset """
-
-
-print("Loading dataset with input sequence length {} and output sequence length {}...".format(
+    print("Loading dataset with input sequence length {} and output sequence length {}...".format(
         config['len_inp_sequence'], config['len_out_sequence']))
 
-dataset = CustomDataset(
-    config['data_path'],
-    transform=transforms.Compose([
-        transforms.Grayscale(),
-        transforms.ToTensor()
-    ]),
-    len_inp_sequence=config['len_inp_sequence'],
-    len_out_sequence=config['len_out_sequence'],
-    load_to_ram=config['load_data_to_ram'],
-    question=config['use_question'],
-    load_ground_truth=False,
-    load_config=True
-)
-
-
-if config['batch_size'] > len(dataset):
-    raise Exception('Batch size bigger than the dataset.')
-
-if config['do_overfitting']:
-    print("Overfitting on a subset of {} samples".format(config['num_train_overfit']))
-    if config['batch_size'] > config['num_train_overfit']:
-        raise Exception('Batchsize for overfitting bigger than the number of samples for overfitting.')
-    else:
-        train_data_sampler = SequentialSampler(range(config['num_train_overfit']))
-        val_data_sampler = SequentialSampler(range(config['num_train_overfit']))
-
-else:
-    print("Training on {} samples".format(config['num_train_regular']))
-    if config['num_train_regular']+config['num_val_regular'] > len(dataset):
-        raise Exception(
-            'Trying to use more samples for training and validation than len(dataset), {} > {}.'.format(
-                config['num_train_regular'] + config['num_val_regular'], len(dataset)
-            ))
-    else:
-        train_data_sampler = SubsetRandomSampler(range(config['num_train_regular']))
-        val_data_sampler = SubsetRandomSampler(range(
-            config['num_train_regular'],
-            config['num_train_regular']+config['num_val_regular']
-        ))
-
-
-train_data_loader = torch.utils.data.DataLoader(
-    dataset=dataset,
-    batch_size=config['batch_size'],
-    num_workers=config['num_workers'],
-    sampler=train_data_sampler,
-    drop_last=True,
-    **kwargs
-)
-val_data_loader = torch.utils.data.DataLoader(
-    dataset=dataset,
-    batch_size=config['batch_size'],
-    num_workers=config['num_workers'],
-    sampler=val_data_sampler,
-    drop_last=True,
-    **kwargs
-)
-
-
-""" Initialize model and solver """
-
-if config['continue_training']:
-    print("Continuing training with model: {} and solver: {}".format(
-        config['model_path'], config['solver_path'])
-    )
-
-    model = torch.load(config['model_path'])
-    model.to(device)
-    solver = Solver()
-    solver.optim = torch.optim.Adam(model.parameters(), lr=config['learning_rate'])
-    solver.load(config['solver_path'], device=device)
-    optimizer = None
-
-else:
-    print("Initializing model...")
-    model = VariationalAutoEncoder(
-        len_in_sequence=config['len_inp_sequence'],
+    dataset = CustomDataset(
+        config['data_path'],
+        transform=transforms.Compose([
+            transforms.Grayscale(),
+            transforms.ToTensor()
+        ]),
+        len_inp_sequence=config['len_inp_sequence'],
         len_out_sequence=config['len_out_sequence'],
-        z_dim_encoder=config['z_dim_encoder'],
-        z_dim_decoder=config['z_dim_decoder'],
-        use_physics=config['use_physics']
+        load_to_ram=config['load_data_to_ram'],
+        question=config['use_question'],
+        load_ground_truth=False,
+        load_config=True
     )
-    solver = Solver()
-    optimizer = torch.optim.Adam(model.parameters(), lr=config['learning_rate'])
 
-""" Perform training """
+    if config['batch_size'] > len(dataset):
+        raise Exception('Batch size bigger than the dataset.')
 
-if __name__ == "__main__":
+    if config['do_overfitting']:
+        print("Overfitting on a subset of {} samples".format(config['num_train_overfit']))
+        if config['batch_size'] > config['num_train_overfit']:
+            raise Exception('Batchsize for overfitting bigger than the number of samples for overfitting.')
+        else:
+            train_data_sampler = SequentialSampler(range(config['num_train_overfit']))
+            val_data_sampler = SequentialSampler(range(config['num_train_overfit']))
+
+    else:
+        print("Training on {} samples".format(config['num_train_regular']))
+        if config['num_train_regular'] + config['num_val_regular'] > len(dataset):
+            raise Exception(
+                'Trying to use more samples for training and validation than len(dataset), {} > {}.'.format(
+                    config['num_train_regular'] + config['num_val_regular'], len(dataset)
+                ))
+        else:
+            train_data_sampler = SubsetRandomSampler(range(config['num_train_regular']))
+            val_data_sampler = SubsetRandomSampler(range(
+                config['num_train_regular'],
+                config['num_train_regular'] + config['num_val_regular']
+            ))
+
+    train_data_loader = torch.utils.data.DataLoader(
+        dataset=dataset,
+        batch_size=config['batch_size'],
+        num_workers=config['num_workers'],
+        sampler=train_data_sampler,
+        drop_last=True,
+        **kwargs
+    )
+    val_data_loader = torch.utils.data.DataLoader(
+        dataset=dataset,
+        batch_size=config['batch_size'],
+        num_workers=config['num_workers'],
+        sampler=val_data_sampler,
+        drop_last=True,
+        **kwargs
+    )
+
+    """ Initialize model and solver """
+
+    if config['continue_training']:
+        print("Continuing training with model: {} and solver: {}".format(
+            config['model_path'], config['solver_path'])
+        )
+
+        model = torch.load(config['model_path'])
+        model.to(device)
+        solver = Solver()
+        solver.optim = torch.optim.Adam(model.parameters(), lr=config['learning_rate'])
+        solver.load(config['solver_path'], device=device)
+        optimizer = None
+
+    else:
+        print("Initializing model...")
+        model = VariationalAutoEncoder(
+            len_in_sequence=config['len_inp_sequence'],
+            len_out_sequence=config['len_out_sequence'],
+            z_dim_encoder=config['z_dim_encoder'],
+            z_dim_decoder=config['z_dim_decoder'],
+            use_physics=config['use_physics']
+        )
+        solver = Solver()
+        optimizer = torch.optim.Adam(model.parameters(), lr=config['learning_rate'])
+
+    """ Perform training """
     solver.train(model=model,
                  train_config=config,
                  dataset_config=dataset.config,
@@ -203,3 +195,7 @@ if __name__ == "__main__":
                  gamma=config['gamma'],
                  target_var=config['target_var'],
                  log_reconstructed_images=config['log_reconstructed_images'])
+
+
+if __name__ == "__main__":
+    train(config)
